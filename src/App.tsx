982ebd67d0b06f81cf02ddfd3d69154104013ecb
alangh <gmartinialan@gmail.com>
2022-05-12 12:09:29 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Botao from './Components/Botao';
 function App() {
+
+  const renderButton = () => (
+    <button>This is a button</button>
+  );
+
+  const renderCloseButton = () => (
+    <button>This is a close ubtton</button>
+  );
+
+  let isButton = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+
+        <p>Algo</p>
+          {isButton ? <Botao /> : renderCloseButton()}
+        <h3 className="">Olá!</h3>
+      
+  </div>
   );
 }
 
---
+++ src/Components/Botao/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Botao() { 
+
+    return (
+        <div className="">
+            <button>This is a Button Component</button>
+        </div>
+    )
+}
+
+
+
+export default Botao;
